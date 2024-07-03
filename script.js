7c1534fc5ee5097ea012ccbdb9dbc40c68626aa8
Kimberly Souza <kimberlydesouza0810@gmail.com>
2024-07-02 21:44:52 -0300

    Inclusão de JV e mouseover nos cards

---
+++ script.js
@@ -0,0 +1,4 @@
+const cartao = document.querySelector('.minhasPaixoes__cards__cartao')
+
+cartao.addEventListener('mouseover', () => {
+	cartao.innerHTML = "Lorem ipsum dolor sit amet. Sed iure omnis qui porro aliquam et assumenda perferendis id laborum error. Sed blanditiis repellendus et maiores aspernatur ea eaque unde qui ullam corporis est tempora esse. Non beatae dolorem hic quod sunt in aperiam excepturi aut quas cupiditate?" });
