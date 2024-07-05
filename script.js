702bc7cfe1c440efcdc5a5653c0979d093635177
Kimberly Souza <kimberlydesouza0810@gmail.com>
2024-07-05 18:55:04 -0300

    Inclusao de evento para adicionar texto nos cards

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const cartao = document.querySelector('.minhasPaixoes__cards__cartao')
+const cartaoBeleza = document.querySelector('#card__beleza')
+const cartaoMusica = document.querySelector('#card__musica')
+const cartaoJogos = document.querySelector('#card__jogos')
+const cartaoTecnologia = document.querySelector('#card__tecnologia')
 
-cartao.addEventListener('mouseover', () => {
-	cartao.innerHTML = "Lorem ipsum dolor sit amet. Sed iure omnis qui porro aliquam et assumenda perferendis id laborum error. Sed blanditiis repellendus et maiores aspernatur ea eaque unde qui ullam corporis est tempora esse. Non beatae dolorem hic quod sunt in aperiam excepturi aut quas cupiditate?" });
+function textoBeleza () {
+	cartaoBeleza.innerHTML = "Lorem ipsum dolor sit amet. Sed iure omnis qui porro aliquam et assumenda perferendis id laborum error. "
+}
+
+function textoMusica () {
+	cartaoMusica.innerHTML = "Lorem ipsum dolor sit amet. Sed iure omnis qui porro aliquam et assumenda perferendis id laborum error. "
+}
+
+function textoJogos () {
+	cartaoJogos.innerHTML = "Lorem ipsum dolor sit amet. Sed iure omnis qui porro aliquam et assumenda perferendis id laborum error. "
+}
+
+function textoTecnologia () {
+	cartaoTecnologia.innerHTML = "Lorem ipsum dolor sit amet. Sed iure omnis qui porro aliquam et assumenda perferendis id laborum error. "
+}
+
+function adicionaTexto () {
+	cartaoBeleza.addEventListener('mouseover', () => {textoBeleza()})
+	cartaoMusica.addEventListener('mouseover', () => {textoMusica()})
+	cartaoJogos.addEventListener('mouseover', () => {textoJogos()})
+	cartaoTecnologia.addEventListener('mouseover', () => {textoTecnologia()})
+}
+
+adicionaTexto()
